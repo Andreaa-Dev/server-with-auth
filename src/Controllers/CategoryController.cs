c1d5d743716d83c741f40df2526e34bfd1a8f4ac
andrea <ngocanhhsgs@gmail.com>
2024-08-12 21:28:20 +0300

    category done

--- src/Controllers/CategoryController.cs
+++ src/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 using Backend.src.Controller;
 using Backend.src.DTO;
 using Backend.src.Service.Impl;
+using Backend.src.Shared;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Backend.src.Controller
@@ -18,9 +19,36 @@ namespace Backend.src.Controller
         public async Task<ActionResult<CategoryReadDto>> CreateOneAsync([FromBody] CategoryCreateDto createDto)
         {
             var categoryCreated = await _categoryService.CreateOneAsync(createDto);
-            Console.WriteLine($"smt {categoryCreated}");
             return Ok(categoryCreated);
         }
 
+        [HttpGet()]
+        public async Task<ActionResult<IEnumerable<CategoryReadDto>>> GetAllAsync([FromQuery] GetAllOptions getAllOptions)
+        {
+            var categoryList = await _categoryService.GetAllAsync(getAllOptions);
+            return Ok(categoryList);
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<CategoryReadDto>> GetByIdAsync([FromRoute] Guid id)
+        {
+            var category = await _categoryService.GetByIdAsync(id);
+            return Ok(category);
+        }
+
+        [HttpPatch("{id:guid}")]
+        public async Task<ActionResult<bool>> UpdateOneAsync([FromRoute] Guid id, CategoryUpdateDto updateDto)
+        {
+            var isUpdated = await _categoryService.UpdateOneAsync(id, updateDto);
+            return Ok(isUpdated);
+        }
+        // id:guid => type of guid
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<bool>> DeleteOneAsync([FromRoute] Guid id)
+        {
+            var isDeleted = await _categoryService.DeleteOneASync(id);
+            System.Console.WriteLine(isDeleted);
+            return Ok(isDeleted);
+        }
     }
 }
