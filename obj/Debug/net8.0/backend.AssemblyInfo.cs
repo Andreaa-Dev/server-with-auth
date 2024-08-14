f2dfa99834a47b6c432bdd9c7badfd23306ff396
andrea <ngocanhhsgs@gmail.com>
2024-08-15 00:06:36 +0300

    user sign up done

--- obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19939d9d65773c1e7d26b8f4b236d9de9be73b7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e123864de73a2b38d68dc847acc0babd7f07d61")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
